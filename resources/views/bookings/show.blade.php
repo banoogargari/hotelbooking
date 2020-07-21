928856d7d55142fc304144ca95c76853b2f0c93e
banoogargari <47190604+banoogargari@users.noreply.github.com>
2020-07-21 13:36:26 -0700

    my project

---
+++ resources/views/bookings/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<dl>
+    @foreach($booking->getAttributes() as $name => $value)
+        <dt>{{ $name }}</dt>
+        <dd>{{ $booking->$name }}</dd>
+    
+    @endforeach
+</dl>
+@endsection
